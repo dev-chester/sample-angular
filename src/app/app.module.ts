520bac22bd7cc1472ba923af0360ae4bfdd43c01
dev-chester <chester.arellano@apptech-experts.com>
2020-03-29 01:24:47 +0800

    modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";  
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {InputTextModule} from 'primeng/inputtext';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
